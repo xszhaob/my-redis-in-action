1f19b62f6e371766cf82167c9927a49261cb5273
xszhaobo <azhaobo1991@163.com>
2016-09-06 23:43:33 +0800

    使用redis存储统计数据，加注释并测试

--- src/main/java/chapter05/UpdateStats.java
+++ src/main/java/chapter05/UpdateStats.java
@@ -1,14 +1,12 @@
 package chapter05;
 
+import org.junit.Test;
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.Pipeline;
 import redis.clients.jedis.Transaction;
 import redis.clients.jedis.ZParams;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.TimeZone;
 import java.util.UUID;
 
 /**
@@ -22,6 +20,15 @@ import java.util.UUID;
  */
 public class UpdateStats {
 
+    @Test
+    public void testUpdateStats() {
+        Jedis conn = new Jedis("localhost");
+        conn.select(15);
+        UpdateStats updateStats = new UpdateStats();
+        for (int i = 0; i < 100; i++) {
+            updateStats.updateStats(conn, "hit", "counter", i, 1000);
+        }
+    }
 
     private List<Object> updateStats(Jedis conn, String context, String type, int value, long timeOut) {
         String zKey = "stats:" + context + ":" + type;
@@ -33,33 +40,46 @@ public class UpdateStats {
 
         while (System.currentTimeMillis() < end) {
             Transaction trans = conn.multi();
+            // 如果key时上个小时的，那么先归档，然后重新设置当前小时的key
             if (existing != null && Chapter05.COLLATOR.compare(existing, start) < 0) {
-                trans.rename(zKey,zKey + ":last");
-                trans.rename(startKey,zKey + ":pstart");
+                trans.rename(zKey, zKey + ":last");
+                trans.rename(startKey, zKey + ":pstart");
             }
-            trans.set(startKey,start);
+            trans.set(startKey, start);
 
+            // 设置临时值
             String tempKeyMax = UUID.randomUUID().toString();
             String tempKeyMin = UUID.randomUUID().toString();
 
             trans.zadd(tempKeyMax, value, "max");
             trans.zadd(tempKeyMax, value, "min");
 
+            // 获取并集，并且分值以最大、最小的为准
+            /*
+            使用聚合函数MIN和MAX，
+            对存储统计数据的键以及两个临时键进行并集计算
+             */
             trans.zunionstore(zKey,
                     new ZParams().aggregate(ZParams.Aggregate.MAX),
                     zKey, tempKeyMax);
             trans.zunionstore(zKey, new ZParams().aggregate(ZParams.Aggregate.MIN),
                     zKey, tempKeyMin);
-            trans.del(tempKeyMax,tempKeyMin);
-            trans.zincrby(zKey,1,"count");
-            trans.zincrby(zKey,value,"sum");
-            trans.zincrby(zKey,value * value,"sumsq");
+            // 删除两个临时key
+            trans.del(tempKeyMax, tempKeyMin);
+            // 总数加1
+            trans.zincrby(zKey, 1, "count");
+            // 和加上本次值
+            trans.zincrby(zKey, value, "sum");
+            // 平方和加上本次值
+            trans.zincrby(zKey, value * value, "sumsq");
 
             List<Object> exec = trans.exec();
+            System.out.println(exec);
             if (exec == null) {
                 continue;
             }
-            return exec.subList(exec.size() - 3,exec.size());
+            System.out.println(conn.get(startKey));
+            return exec.subList(exec.size() - 3, exec.size());
         }
         return null;
     }
