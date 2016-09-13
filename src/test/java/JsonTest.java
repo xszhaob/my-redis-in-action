0acef490e9fe0d94ca1f2b4fa969d402254219e6
xszhaobo <azhaobo1991@163.com>
2016-09-13 23:11:59 +0800

    format code

--- src/test/java/JsonTest.java
+++ src/test/java/JsonTest.java
@@ -1,5 +1,3 @@
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
