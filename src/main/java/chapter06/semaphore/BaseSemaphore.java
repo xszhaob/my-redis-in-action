6f36d6256b0c61c7d82f2e69e466c225aeeb61b7
xszhaobo <azhaobo1991@163.com>
2016-09-17 00:07:45 +0800

    利用redis实现的基本信号量

--- src/main/java/chapter06/semaphore/BaseSemaphore.java
+++ src/main/java/chapter06/semaphore/BaseSemaphore.java
@@ -18,22 +18,39 @@ import java.util.UUID;
  */
 public class BaseSemaphore {
 
-
-    private String acquireSemaphore(Jedis conn,String semName,int limit,long timeOut) {
+    /**
+     * 获取一个信号量
+     *
+     * @param conn    redis连接
+     * @param semName 信号量名称
+     * @param limit   信号量限制数
+     * @param timeOut 获取的信号量过期时间
+     * @return 信号量标识
+     */
+    private String acquireSemaphore(Jedis conn, String semName, int limit, long timeOut) {
         String member = UUID.randomUUID().toString();
         long now = System.currentTimeMillis();
 
         Transaction trans = conn.multi();
         // 先删除过期的信号量
-        trans.zremrangeByScore(semName,0,now - timeOut);
-        trans.zadd(semName,now,member);
-        trans.zrank(semName,member);
+        trans.zremrangeByScore(semName, 0, now - timeOut);
+        trans.zadd(semName, now, member);
+        trans.zrank(semName, member);
         List<Object> exec = trans.exec();
-        if (exec != null && (Long)(exec.get(exec.size() - 1)) < limit) {
+        if (exec != null && (Long) (exec.get(exec.size() - 1)) < limit) {
             return member;
         }
         return null;
     }
 
-
+    /**
+     * 删除信号量
+     *
+     * @param conn    redis连接
+     * @param semName 信号量名称
+     * @param id      信号量标识
+     */
+    private void releaseSemaphore(Jedis conn, String semName, String id) {
+        conn.zrem(semName, id);
+    }
 }
