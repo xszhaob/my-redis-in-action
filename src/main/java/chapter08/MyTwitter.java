bb2347128ab1b929baf8c420de2410ce6392b6b9
xszhaobo <azhaobo1991@163.com>
2016-10-26 23:25:20 +0800

    更新关注列表，获取时间线状态

--- src/main/java/chapter08/MyTwitter.java
+++ src/main/java/chapter08/MyTwitter.java
@@ -1,13 +1,9 @@
 package chapter08;
 
 import chapter06.lock.BetterLock;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.Transaction;
+import redis.clients.jedis.*;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * author:xszhaobo
@@ -28,6 +24,7 @@ public class MyTwitter {
     private static final String FOLLOWING_KEY = "following:";
     private static final String USER_ID = "user:id:";
     private static final String STATUS_ID = "status:id:";
+    private static final long HOME_TIMELINE_SIZE = 1000;
     private static final BetterLock LOCK = new BetterLock();
 
     public String createUser(Jedis conn, String login, String name) {
@@ -78,11 +75,89 @@ public class MyTwitter {
         data.put("posted", System.currentTimeMillis() + "");
         data.put("id", execRet.get(1).toString());
         data.put("uid", uid);
+        data.put("delete", "false");
         data.put("login", execRet.get(0).toString());
         trans = conn.multi();
         trans.hmset(STATUS_KEY + execRet.get(1), data);
         trans.hincrBy(USER_KEY + uid, "posts", 1);
         trans.exec();
         return execRet.get(1).toString();
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Map<String,String>> getStatusMessage(Jedis conn,String uid,String timeLine,int page,int count) {
+        if (page == 0) {
+            page = 1;
+        }
+        if (count == 0) {
+            count = 30;
+        }
+        if (timeLine == null || timeLine.isEmpty()) {
+            timeLine = HOME_KEY;
+        }
+
+        Set<String> statusSet = conn.zrevrange(timeLine + uid, ((page - 1) * count), page * count - 1);
+        if (statusSet == null || statusSet.isEmpty()) {
+            return new ArrayList<Map<String, String>>();
+        }
+        List<Map<String,String>> resultList = new ArrayList<Map<String, String>>(statusSet.size());
+        Transaction multi = conn.multi();
+        for (String s : statusSet) {
+            multi.hgetAll(STATUS_KEY + s);
+        }
+        List<Object> exec = multi.exec();
+        for (Object o : exec) {
+            Map<String,String> status = (Map<String,String>) o;
+            if (!isDelete(status)) {
+                resultList.add(status);
+            }
+        }
+        return resultList;
+    }
+
+
+    private boolean isDelete(Map<String,String> status) {
+        try {
+            return Boolean.parseBoolean(status.get("delete"));
+        } catch (Exception ignore) {
+            return true;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public boolean followUser(Jedis conn,String uid,String followUserId) {
+        String followingKey = FOLLOWING_KEY + uid;
+        String followerKey = FOLLOWERS_KEY + followUserId;
+        if (conn.zscore(followingKey,followUserId) != null) {
+            return false;
+        }
+        long nowTimeLong = System.currentTimeMillis();
+        Transaction trans = conn.multi();
+        trans.zadd(followingKey,nowTimeLong,followUserId);
+        trans.zadd(followerKey,nowTimeLong,uid);
+
+        trans.zrevrangeWithScores(STATUS_KEY + followUserId, 0, HOME_TIMELINE_SIZE - 1);
+        List<Object> exec = trans.exec();
+        if (exec == null) {
+            return false;
+        }
+        trans = conn.multi();
+        trans.hincrBy(USER_KEY + uid,"following",Long.parseLong(exec.get(0).toString()));
+        trans.hincrBy(USER_KEY + uid,"followers",Long.parseLong(exec.get(1).toString()));
+        Set<Tuple> followingStatus = (Set<Tuple>) exec.get(2);
+        if (followingStatus != null) {
+            trans.zadd(HOME_KEY + uid,getScoreMembers(followingStatus));
+        }
+        trans.zremrangeByRank(HOME_KEY + uid,0,-HOME_TIMELINE_SIZE -1);
+        trans.exec();
+        return true;
+    }
+
+    private Map<String,Double> getScoreMembers(Set<Tuple> tuples) {
+        Map<String,Double> result = new HashMap<String, Double>();
+        for (Tuple tuple : tuples) {
+            result.put(tuple.getElement(),tuple.getScore());
+        }
+        return result;
     }
 }
