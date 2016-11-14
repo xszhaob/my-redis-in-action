e93c8ad6904a7c05100882300a38d207f0a2fa17
zhaobo <1991417zhao>
2016-11-15 00:07:48 +0800

    分片计算

---
+++ src/main/java/chapter09/ShardTest.java
@@ -0,0 +1,36 @@
+package chapter09;
+
+import java.util.zip.CRC32;
+
+/**
+ * Created by zhaobo on 2016/11/14.
+ */
+public class ShardTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String shardKey(String base,String key,long totalElements,int shardSize) {
+        long shardId = 0;
+        if (isDigit(key)) {
+            shardId = Integer.parseInt(key,10) / shardSize;
+        } else {
+            CRC32 crc = new CRC32();
+            crc.update(key.getBytes());
+            long shards = 2 * totalElements / shardSize;
+            shardId = Math.abs((int) crc.getValue() % shards);
+        }
+        return base + ":" + shardId;
+    }
+
+
+    private boolean isDigit(String str) {
+        for (char c : str.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
